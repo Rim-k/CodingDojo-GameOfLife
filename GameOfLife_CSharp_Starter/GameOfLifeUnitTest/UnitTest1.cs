28c71ac58aaa7c59370f11f407795bfc6b2df827
CISSE Abdoul karim <S622888@siege.axa-fr.intraxa>
2016-07-01 17:40:03 +0200

    Adding CSharp starter

---
+++ GameOfLife_CSharp_Starter/GameOfLifeUnitTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StarterCodingDojoGameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace GameOfLifeUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Une_Cellule_Vivante_Avec_Moins_de_2_Voisines_Meurt()
+        {
+            //Arrange
+            CelluleEtat cellule = CelluleEtat.Vivante;
+            int nombreVoisin = 1;
+
+            //Act
+            CelluleEtat resultat = VieDuneCellule.NouvelleEtat(cellule, nombreVoisin);
+
+            //Assert 
+            Assert.AreEqual(CelluleEtat.Morte, resultat);
+        }
+
+        [TestMethod]
+        public void Une_Cellule_Vivante___()
+        {
+            //Arrange 
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
+
---
+++ GameOfLife_CSharp_Starter/StarterCodingDojoGameOfLife/VieDuneCellule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterCodingDojoGameOfLife
+{
+    public enum CelluleEtat
+    {
+        Vivante,
+        Morte
+    }
+
+    public class VieDuneCellule
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding Dojo Game Of Life");
+
+            Console.ReadLine();
+        }
+
+        public static CelluleEtat NouvelleEtat(CelluleEtat cellule, int nbVoisin)
+        {
+                return CelluleEtat.Morte;
+        }
+
+    }
+}
